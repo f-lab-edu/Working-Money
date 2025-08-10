e3851ed792bc44e9424aabb067b0fce4aeeaed0c
low33 <qnm835@gmail.com>
2025-08-10 16:56:19 +0900

    프로젝트 생성

---
+++ src/test/java/org/example/workingmoney/WorkingMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.workingmoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkingMoneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
