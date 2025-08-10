318507d74c0b969643e43807de0f144cf63b1221
low33 <qnm835@gmail.com>
2025-08-10 21:29:35 +0900

    Chore/#5 프로젝트 생싱 및 초기 세팅 (#6)
    * 프로젝트 생성
    * database 설정

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.4"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "org.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Spring Boot Core
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    
+    // Database
+    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.5")
+    runtimeOnly("com.mysql:mysql-connector-j")
+    
+    // Development Tools
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    
+    // Testing
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.mybatis.spring.boot:mybatis-spring-boot-starter-test:3.0.5")
+    testImplementation("org.springframework.security:spring-security-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/java/org/example/workingmoney/WorkingMoneyApplication.java
@@ -0,0 +1,13 @@
+package org.example.workingmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingMoneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkingMoneyApplication.class, args);
+    }
+
+}
