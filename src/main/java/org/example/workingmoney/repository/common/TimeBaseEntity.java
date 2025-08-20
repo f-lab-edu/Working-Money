e2b2c066bd682bb986bfaf3a12574e6323c8cbdb
low33 <qnm835@gmail.com>
2025-08-20 19:59:07 +0900

    TimeBaseEntity abstract class로 수정

--- src/main/java/org/example/workingmoney/repository/common/TimeBaseEntity.java
+++ src/main/java/org/example/workingmoney/repository/common/TimeBaseEntity.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 import java.time.LocalDateTime;
 
 @Getter
-public class TimeBaseEntity {
+public abstract class TimeBaseEntity {
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 }
